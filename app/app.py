97145d89995c0af080e1ced28974319fd164458c
Lucas Pollice <lucas.pollice@nyu.edu>
2018-08-17 12:39:16 -0400

    minor change

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ from itsdangerous import TimedJSONWebSignatureSerializer as JWT
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = 'the quick brown fox jumps over the lazy dog'
-app.config["DATABASE_URI"] = 'mysql://root:Minecraft700@localhost/store'
+app.config["DATABASE_URI"] = 'mysql://root:password@localhost/store'
 app.config["WIPE"] = False
 jwt = JWT(app.config['SECRET_KEY'], expires_in=3600)
 socketio = SocketIO(app)
